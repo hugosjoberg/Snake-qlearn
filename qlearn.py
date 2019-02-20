748015ca8499d056c74d8a72fde46c6e5e9e3320
Hugo Sjöberg <hugo.sjoberg88@gmail.com>
2019-02-20 20:59:50 +0100

    Ran black over the code

--- qlearn.py
+++ qlearn.py
@@ -1,97 +1,101 @@
 import snake as game
 import argparse
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import SGD , Adam
+from keras.layers import Dense, Activation, Flatten, Convolution2D
+from keras.optimizers import Adam
 import numpy as np
 import skimage as skimage
-from skimage import transform, color, exposure
-from skimage.transform import rotate
-from skimage.viewer import ImageViewer
-from collections import deque
 import random
 import json
 from random import sample as rsample
-from keras.optimizers import RMSprop
 import time
-import sys
 
-INPUT_SHAPE=(80,80,2) #Shape of the image imported into the NN
-NB_ACTIONS = 5 #NB_ACTIONS is the number of actions the player can do in the game
+INPUT_SHAPE = (80, 80, 2)  # Shape of the image imported into the NN
+NB_ACTIONS = 5  # NB_ACTIONS is the number of actions the player can do
 BATCH = 100
-GAME_INPUT = [0,1,2,3,4]
+GAME_INPUT = [0, 1, 2, 3, 4]
 EPSILON = 1
 EPSILON_DECAY = 0.99
 FINAL_EPSILON = 0.3
 LEARNING_RATE = 1e-4
 GAMMA = 0.7
 NB_FRAMES = 1
-#Building a NN model for interpretating the image from the game
-#The architecture is borrowed from https://github.com/matthiasplappert/keras-rl/blob/master/examples/dqn_atari.py
-#First wanted to try the DeepMind Atari model but comments recommended the one below instead
-#Performance is supposed to be a lot better.
+
+# Building a NN model for interpretating the image from the game
+# The architecture is borrowed from
+# https://github.com/matthiasplappert/keras-rl/blob/master/examples/dqn_atari.py
+# First wanted to try the DeepMind Atari model but
+# comments recommended the one below instead
+# Performance is supposed to be a lot better.
+
 
 def build_model():
     model = Sequential()
 
-    model.add(Convolution2D(16, (8, 8), strides=(4, 4),input_shape=INPUT_SHAPE))
-    model.add(Activation('relu'))
+    model.add(Convolution2D(16, (8, 8), strides=(4, 4), input_shape=INPUT_SHAPE))
+    model.add(Activation("relu"))
     model.add(Convolution2D(32, (4, 4), strides=(2, 2)))
-    model.add(Activation('relu'))
+    model.add(Activation("relu"))
     model.add(Flatten())
     model.add(Dense(256))
     model.add(Dense(NB_ACTIONS))
 
-    #I chosed to use a Adam optimizer, I have used it before with good results
-    adam=Adam(lr=LEARNING_RATE)
-    model.compile(loss='mean_squared_error',
-                    optimizer=adam)
+    # I chosed to use a Adam optimizer, I have used it before with good results
+    adam = Adam(lr=LEARNING_RATE)
+    model.compile(loss="mean_squared_error", optimizer=adam)
     print(model.summary())
     return model
+
 
 def experience_replay(batch_size):
     memory = []
     while True:
-        experience = yield rsample(memory, batch_size) if batch_size <= len(memory) else None
+        experience = (
+            yield rsample(memory, batch_size) if batch_size <= len(memory) else None
+        )
         memory.append(experience)
 
+
 def stack_image(game_image):
-    #Make image black and white
+    # Make image black and white
     x_t = skimage.color.rgb2gray(game_image)
-    #Resize the image to 80x80 pixels
-    x_t = skimage.transform.resize(x_t,(80,80))
-    #Change the intensity of colors, maximizing the intensities.
-    x_t = skimage.exposure.rescale_intensity(x_t,out_range=(0,255))
+    # Resize the image to 80x80 pixels
+    x_t = skimage.transform.resize(x_t, (80, 80))
+    # Change the intensity of colors, maximizing the intensities.
+    x_t = skimage.exposure.rescale_intensity(x_t, out_range=(0, 255))
     # Stacking 2 images for the agent to get understanding of speed
-    s_t = np.stack((x_t,x_t),axis=2)
+    s_t = np.stack((x_t, x_t), axis=2)
     # Reshape to make keras like it
     s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])
     return s_t
 
+
 def nn_loadOld_weights(model):
-    print ("Now we load weight")
+    print("Now we load weight")
     model.load_weights("model.h5")
-    print ("Weight load successfully")
-    print ("Let the training begin!")
+    print("Weight load successfully")
+    print("Let the training begin!")
     train_network(model)
+
 
 def train_network(model):
     epsilon = EPSILON
-    game_state = game.Game() #Starting up a game
+    game_state = game.Game()  # Starting up a game
     game_state.set_start_state()
-    game_image,score,game_lost = game_state.run(0) #The game is started but no action is performed
+    game_image, score, game_lost = game_state.run(
+        0
+    )  # The game is started but no action is performed
     s_t = stack_image(game_image)
     terminal = False
     t = 0
     exp_replay = experience_replay(BATCH)
     exp_replay.__next__()  # Start experience replay coroutine
-    while(True):
+    while True:
         loss = 0
         Q_sa = 0
         action_index = 4
         r_t = 0
-        a_t = 'no nothing'
+        a_t = "no nothing"
         if terminal:
             game_state.set_start_state()
         if t % NB_FRAMES == 0:
@@ -105,7 +109,7 @@ def train_network(model):
             epsilon = epsilon * EPSILON_DECAY
         else:
             epsilon = FINAL_EPSILON
-        #run the selected action and observed next state and reward
+        # run the selected action and observed next state and reward
         x_t1_colored, r_t, terminal = game_state.run(a_t)
         s_t1 = stack_image(x_t1_colored)
         experience = (s_t, a_t, r_t, s_t1)
@@ -115,16 +119,16 @@ def train_network(model):
             inputs = np.zeros((BATCH, s_t.shape[1], s_t.shape[2], s_t.shape[3]))
             targets = np.zeros((BATCH, NB_ACTIONS))
             i = 0
-            for s,a,r,s_pred in batch:
-                inputs[i:i + 1] = s
+            for s, a, r, s_pred in batch:
+                inputs[i : i + 1] = s
                 if r < 0:
-                    targets[i ,a] = r
+                    targets[i, a] = r
                 else:
                     Q_sa = model.predict(s_pred)
-                    targets[i ,a] = r + GAMMA * np.max(Q_sa)
-                i+=1
-            loss += model.train_on_batch(inputs,targets)
-            #Exploration vs Exploitation
+                    targets[i, a] = r + GAMMA * np.max(Q_sa)
+                i += 1
+            loss += model.train_on_batch(inputs, targets)
+            # Exploration vs Exploitation
 
         t += 1
         # save progress every 10000 iterations
@@ -135,26 +139,39 @@ def train_network(model):
                 json.dump(model.to_json(), outfile)
 
         if t % 500 == 0:
-
-            print("TIMESTEP", t, \
-                "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
-                "/ Q_MAX " , np.max(Q_sa), "/ Loss ", loss)
+            print(
+                "TIMESTEP",
+                t,
+                "/ EPSILON",
+                epsilon,
+                "/ ACTION",
+                action_index,
+                "/ REWARD",
+                r_t,
+                "/ Q_MAX ",
+                np.max(Q_sa),
+                "/ Loss ",
+                loss,
+            )
 
     print("Episode finished!")
     print("************************")
 
+
 def nn_playGame(model):
-    print ("Now we load weight")
+    print("Now we load weight")
     model.load_weights("model.h5")
-    print ("Weight load successfully")
-    print ("Let the game begin!")
-    game_state = game.Game() #Starting up a game
+    print("Weight load successfully")
+    print("Let the game begin!")
+    game_state = game.Game()  # Starting up a game
     game_state.set_start_state()
-    game_image,score,game_lost = game_state.run(4) #The game is started but no action is performed
+    game_image, score, game_lost = game_state.run(
+        4
+    )  # The game is started but no action is performed
     s_t = stack_image(game_image)
     s_t1 = s_t
     a_t = 4
-    while(True):
+    while True:
 
         if game_lost:
             print("Game lost")
@@ -168,13 +185,14 @@ def nn_playGame(model):
         s_t1 = stack_image(x_t1_colored)
         game_lost = terminal
 
+
 def playGame(args):
     model = build_model()
-    if args['mode'] == "Run":
+    if args["mode"] == "Run":
         nn_playGame(model)
-    elif args['mode'] == "Re-train":
+    elif args["mode"] == "Re-train":
         nn_loadOld_weights(model)
-    elif args['mode'] == "Train":
+    elif args["mode"] == "Train":
         train_network(model)
     else:
         print("*** Not valid argument ***")
@@ -183,9 +201,12 @@ def playGame(args):
         print("Train to train train from scratch")
         print("*********************************")
 
+
 def main():
-    parser = argparse.ArgumentParser(description='How you would like your program to run')
-    parser.add_argument('-m','--mode',help='Train / Run / Re-train', required =True)
+    parser = argparse.ArgumentParser(
+        description="How you would like your program to run"
+    )
+    parser.add_argument("-m", "--mode", help="Train / Run / Re-train", required=True)
     args = vars(parser.parse_args())
     playGame(args)
 
--- snake.py
+++ snake.py
@@ -1,136 +1,191 @@
-import pygame, random, sys, time, math, random
-from pygame.locals import *
+import pygame
+import random
+import time
+import math
+
 
 class Game(object):
+    def __init__(self):
+        pygame.init()
+        self.height = 400
+        self.width = 400
+        self.border_width = 10
+        self.s = pygame.display.set_mode((self.height, self.width))
+        self.speed = 20
+        self.set_start_state()
 
-	def __init__(self):
-		pygame.init()
-		self.height = 400
-		self.width = 400
-		self.border_width = 10
-		self.s = pygame.display.set_mode((self.height, self.width))
-		self.speed = 20
-		self.set_start_state()
+    def set_start_state(self):
+        self.snake_size = 20
+        self.food_size = 20
+        self.xs = [
+            self.width / 2 - self.snake_size,
+            self.width / 2 - self.snake_size,
+            self.width / 2 - self.snake_size,
+            self.width / 2 - self.snake_size,
+        ]
+        self.ys = [
+            self.height / 2 - self.snake_size,
+            self.height / 2 - 2 * self.snake_size,
+            self.height / 2 - 3 * self.snake_size,
+            self.height / 2 - 4 * self.snake_size,
+        ]
+        self.dirs = random.choice([0, 1, 2, 3])
+        self.score = 0
+        self.applepos = (
+            random.randint(
+                self.border_width + self.food_size,
+                self.height - self.food_size - self.border_width,
+            ),
+            random.randint(
+                self.border_width + self.food_size,
+                self.width - self.food_size - self.border_width,
+            ),
+        )
+        self.appleimage = pygame.Surface((self.food_size, self.food_size))
+        self.appleimage.fill((0, 255, 0))
+        self.img = pygame.Surface((self.food_size, self.food_size))
+        self.img.fill((0, 0, 0))
+        self.game_over = False
+        self.new_distance = 0
+        self.old_distance = 0
 
-	def set_start_state(self):
-		self.snake_size = 20
-		self.food_size = 20
-		self.xs = [self.width/2-self.snake_size, self.width/2-self.snake_size,
-				self.width/2-self.snake_size, self.width/2-self.snake_size]
-		self.ys = [self.height/2-self.snake_size, self.height/2-2*self.snake_size,
-				self.height/2-3*self.snake_size, self.height/2-4*self.snake_size]
-		self.dirs = random.choice([0,1,2,3])
-		self.score = 0
-		self.applepos = (random.randint(self.border_width+self.food_size, self.height-self.food_size-self.border_width),
-		 				random.randint(self.border_width+self.food_size, self.width-self.food_size-self.border_width))
-		self.appleimage = pygame.Surface((self.food_size, self.food_size))
-		self.appleimage.fill((0, 255, 0))
-		self.img = pygame.Surface((self.food_size,self.food_size))
-		self.img.fill((0, 0, 0))
-		self.game_over = False
-		self.new_distance = 0
-		self.old_distance = 0
+    def distance(self, x1, x2, y1, y2):
+        x = math.pow((x1 - x2), 2)
+        y = math.pow((y1 - y2), 2)
+        distance = math.sqrt(x + y)
+        return distance
 
+    def collide(self, x1, x2, y1, y2, w1, w2, h1, h2):
+        if x1 + w1 > x2 and x1 < x2 + w2 and y1 + h1 > y2 and y1 < y2 + h2:
+            return True
+        else:
+            return False
 
-	def distance(self, x1, x2, y1, y2):
-		x = math.pow((x1-x2),2)
-		y = math.pow((y1-y2),2)
-		distance = math.sqrt(x+y)
-		return distance
+    def reward(self, apple_eaten):
+        if self.new_distance < self.old_distance:
+            reward = 0.4
+        else:
+            reward = -0.4
+        if apple_eaten:
+            reward = 1.0
+        return reward
 
-	def collide(self, x1, x2, y1, y2, w1, w2, h1, h2):
-		if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
-			return True
-		else:
-			return False
+    def die(self):
+        reward = -1
+        self.draw_board()
+        image = pygame.surfarray.array3d(pygame.display.get_surface())
+        time.sleep(1 / 10)
+        return image, reward, True
 
-	def reward(self, apple_eaten):
-		if self.new_distance<self.old_distance:
-			reward = 0.4
-		else:
-			reward = -0.4
-		if apple_eaten:
-			reward = 1.0
-		return reward
+    def direction_snake(self, actions):
+        action = actions
+        if action == 2 and self.dirs != 0:
+            self.dirs = 2
+        elif action == 0 and self.dirs != 2:
+            self.dirs = 0
+        elif action == 3 and self.dirs != 1:
+            self.dirs = 3
+        elif action == 1 and self.dirs != 3:
+            self.dirs = 1
+        dirs = self.dirs
+        self.move_snake(dirs)
 
-	def die(self):
-		reward = -1
-		self.draw_board()
-		image = pygame.surfarray.array3d(pygame.display.get_surface())
-		time.sleep(1/10)
-		return image, reward, True
+    def move_snake(self, dirs):
+        if dirs == 0:
+            self.ys[0] += self.speed
+        elif dirs == 1:
+            self.xs[0] += self.speed
+        elif dirs == 2:
+            self.ys[0] -= self.speed
+        elif dirs == 3:
+            self.xs[0] -= self.speed
 
-	def direction_snake(self,actions):
-		action = actions
-		if action == 2 and self.dirs != 0:
-			self.dirs = 2
-		elif action == 0 and self.dirs != 2:
-			self.dirs = 0
-		elif action == 3 and self.dirs != 1:
-			self.dirs = 3
-		elif action == 1 and self.dirs != 3:
-			self.dirs = 1
-		dirs = self.dirs
-		self.move_snake(dirs)
+    def draw_board(self):
+        self.s.fill((255, 255, 255))
+        for i in range(0, len(self.xs)):
+            self.s.blit(self.img, (self.xs[i], self.ys[i]))
+        self.s.blit(self.appleimage, self.applepos)
+        pygame.draw.rect(self.s, (0, 0, 0), [0, 0, self.width, self.border_width])
+        pygame.draw.rect(
+            self.s,
+            (0, 0, 0),
+            [0, self.height - self.border_width, self.width, self.border_width],
+        )
+        pygame.draw.rect(self.s, (0, 0, 0), [0, 0, self.border_width, self.height])
+        pygame.draw.rect(
+            self.s,
+            (0, 0, 0),
+            [self.width - self.border_width, 0, self.border_width, self.height],
+        )
+        pygame.display.update()
 
-	def move_snake(self,dirs):
-		if dirs==0:
-			self.ys[0] += self.speed
-		elif dirs==1:
-			self.xs[0] += self.speed
-		elif dirs==2:
-			self.ys[0] -= self.speed
-		elif dirs==3:
-			self.xs[0] -= self.speed
+    def run(self, actions):
+        actions = actions
+        i = len(self.xs) - 1
+        pygame.event.pump()
+        snake_eat_apple = False
+        self.direction_snake(actions)
+        while i >= 1:
+            self.xs[i] = self.xs[i - 1]
+            self.ys[i] = self.ys[i - 1]
+            i -= 1
+        i = len(self.xs) - 1
+        # Check if snake collide with self
+        while i >= 2:
+            if self.collide(
+                self.xs[0],
+                self.xs[i],
+                self.ys[0],
+                self.ys[i],
+                self.snake_size,
+                self.snake_size,
+                self.snake_size,
+                self.snake_size,
+            ):
+                return self.die()
+            i -= 1
 
-	def draw_board(self):
-		self.s.fill((255, 255, 255))
-		for i in range(0, len(self.xs)):
-			self.s.blit(self.img, (self.xs[i], self.ys[i]))
-		self.s.blit(self.appleimage, self.applepos)
-		pygame.draw.rect(self.s, (0,0,0), [0,0,self.width,self.border_width])
-		pygame.draw.rect(self.s, (0,0,0), [0,self.height-self.border_width,self.width,self.border_width])
-		pygame.draw.rect(self.s, (0,0,0), [0,0,self.border_width,self.height])
-		pygame.draw.rect(self.s, (0,0,0), [self.width-self.border_width,0,self.border_width, self.height])
-		pygame.display.update()
+        # Check if snake collide with apple
+        if self.collide(
+            self.xs[0],
+            self.applepos[0],
+            self.ys[0],
+            self.applepos[1],
+            self.snake_size,
+            self.food_size,
+            self.snake_size,
+            self.food_size,
+        ):
+            self.score += 1
+            self.xs.append(700)
+            self.ys.append(700)
+            self.applepos = (
+                random.randint(
+                    self.border_width + self.food_size,
+                    self.height - self.food_size - self.border_width,
+                ),
+                random.randint(
+                    self.border_width + self.food_size,
+                    self.width - self.food_size - self.border_width,
+                ),
+            )
+            snake_eat_apple = True
 
-	def run(self,actions):
-		actions = actions
-		i = len(self.xs)-1
-		pygame.event.pump()
-		snake_eat_apple = False
-		self.direction_snake(actions)
-		while i >= 1:
-			self.xs[i] = self.xs[i-1]
-			self.ys[i] = self.ys[i-1]
-			i -= 1
-		i = len(self.xs)-1
-		#Check if snake collide with self
-		while i >= 2:
-			if self.collide(self.xs[0], self.xs[i], self.ys[0], self.ys[i], self.snake_size,
-										self.snake_size, self.snake_size, self.snake_size):
-				return self.die()
-			i-= 1
+        # Check if snake collide with wall
+        if (
+            self.xs[0] < self.border_width / 2
+            or self.xs[0] > self.width - self.snake_size - self.border_width / 2
+            or self.ys[0] < self.border_width / 2
+            or self.ys[0] > self.height - self.snake_size - self.border_width / 2
+        ):
+            return self.die()
 
-		#Check if snake collide with apple
-		if self.collide(self.xs[0], self.applepos[0], self.ys[0], self.applepos[1], self.snake_size,
-													self.food_size, self.snake_size, self.food_size):
-			self.score += 1
-			self.xs.append(700)
-			self.ys.append(700)
-			self.applepos = (random.randint(self.border_width+self.food_size, self.height-self.food_size-self.border_width),
-							random.randint(self.border_width+self.food_size, self.width-self.food_size-self.border_width))
-			snake_eat_apple = True
-
-		#Check if snake collide with wall
-		if self.xs[0] < self.border_width/2 or self.xs[0] > self.width-self.snake_size-self.border_width/2 \
-			or self.ys[0] < self.border_width/2 or self.ys[0] > self.height-self.snake_size-self.border_width/2:
-			return self.die()
-
-		#Calculate new distance between snake head and food
-		self.old_distance = self.new_distance
-		self.new_distance = self.distance(self.xs[0], self.applepos[0], self.ys[0], self.applepos[1])
-		self.draw_board()
-		time.sleep(1/20)
-		image = pygame.surfarray.array3d(pygame.display.get_surface())
-		return image, self.reward(snake_eat_apple), self.game_over
+        # Calculate new distance between snake head and food
+        self.old_distance = self.new_distance
+        self.new_distance = self.distance(
+            self.xs[0], self.applepos[0], self.ys[0], self.applepos[1]
+        )
+        self.draw_board()
+        time.sleep(1 / 20)
+        image = pygame.surfarray.array3d(pygame.display.get_surface())
+        return image, self.reward(snake_eat_apple), self.game_over
